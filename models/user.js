e21dbafa093472698372044fad9027e0ef31c6d0
Khallas111 <salimkhorchi11@gmail.com>
2026-03-03 12:44:26 +0100

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Le nom d'utilisateur est requis"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "L'email est requis"],
+      unique: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "L'email est invalide"],
+    },
+    password: {
+      type: String,
+      required: [true, "Le mot de passe est requis"],
+      minlength: [6, "Le mot de passe doit contenir au moins 6 caractères"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
