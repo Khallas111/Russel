7e738978b19f38b03536522b9678d7c28808afc3
Khallas111 <salimkhorchi11@gmail.com>
2026-03-04 10:50:58 +0100

    Swagger
    Implantation de Swagger

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./docs/swagger");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Middleware pour gérer les routes non trouvées (404)
 
 app.use(function (req, res, next) {
   res.status(404).json({
